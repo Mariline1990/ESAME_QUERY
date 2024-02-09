c3831ffc538469871a46c716eb37b42208a8f467
Mariline Luccisano <145480228+Mariline1990@users.noreply.github.com>
2024-02-09 16:40:49 +0100

    Add files via upload

---
+++ SQLQuerYINSERT_1.sql
@@ -0,0 +1,12 @@
+INSERT INTO ANAGRAFICA (COGNOME, NOME, INDIRIZZO, CITTA, CAP, CF, FK_ID_VERBALE)
+VALUES
+    ('Rossi', 'Mario', 'Via Roma 123', 'Roma', '00100', 'RSSMRA80M01H501Z',1),
+    ('Bianchi', 'Laura', 'Corso Vittorio Emanuele 456', 'Milano', '20100', 'BNCLRA75M41H789X',2),
+    ('Verdi', 'Giuseppe', 'Viale dei Fiori 789', 'Napoli', '80100', 'VRDGSF60M05H224F',3),
+    ('Ferrari', 'Anna', 'Piazza Garibaldi 321', 'Torino', '10100', 'FRRNNA85M12H345K',4),
+    ('Russo', 'Luca', 'Largo Mazzini 567', 'Bari', '70100', 'RSSLCU92M08H123S',5),
+    ('Romano', 'Francesca', 'Via Dante 789', 'Firenze', '50100', 'RMNFRN85M13H789A',6),
+    ('Mancini', 'Antonio', 'Corso Umberto 111', 'Palermo', '90100', 'MCNNTN80M02H567G',7),
+    ('Gallo', 'Sara', 'Via XX Settembre 543', 'Genova', '16100', 'GLLSRA89M14H890R',8),
+    ('Conti', 'Marco', 'Piazza Duomo 765', 'Catania', '95100', 'CNTMRC75M23H234D',9),
+    ('Lombardi', 'Elena', 'Vicolo delle Rose 987', 'Venice', '30100', 'LMBELN78M05H123T',10);
---
+++ SQLQuery_INSERT_2.sql
@@ -0,0 +1,12 @@
+INSERT INTO VIOLAZIONE (DESCRIZIONE)
+VALUES
+    ('Eccesso di velocità'),
+    ('Guida in stato di ebbrezza'),
+    ('Attraversamento semaforo rosso'),
+    ('Mancato uso del casco'),
+    ('Telefono cellulare durante la guida'),
+    ('Sorpasso pericoloso'),
+    ('Mancato rispetto della precedenza'),
+    ('Sosta in divieto di fermata'),
+    ('Guida senza cintura di sicurezza'),
+    ('Veicolo senza revisione tecnica');
